88389df537b0d20b6e461e8f1081deead61d92a4
wonnho71 <wonnho@nate.com>
2026-09-05 14:02:41 +0900

    create goodbye.mustache with adding @GetMapping('/bye') in postController using starter template of getbootstrap

--- src/main/java/com/postengine/post/controller/postController.java
+++ src/main/java/com/postengine/post/controller/postController.java
@@ -1,13 +1,21 @@
 package com.postengine.post.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class postController {
 
     @GetMapping("/hi")
-    public String niceToMeetYou() {
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username","everybody");
         return "greetings";// return greetings.mustache
     }
+
+    @GetMapping("/bye")
+    public String byeBye(Model model){
+          model.addAttribute("username","Lioness");
+        return "goodbye";
+      }
 }
