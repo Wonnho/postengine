92f8402df47cf22b288a3830f4a3adaeba3b6503
wonnho71 <wonnho@nate.com>
2026-09-07 13:20:03 +0900

    dummy data, spring.jpa.defer-datasource-initialization=true

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO article (id, subject, content)
+VALUES (1, 'edit', 'edit method');
+
+INSERT INTO article (id, subject, content)
+VALUES (2, 'dummy', 'dummy data');
+
+INSERT INTO article (id, subject, content)
+VALUES (3, 'update', 'action=/articles/update');
