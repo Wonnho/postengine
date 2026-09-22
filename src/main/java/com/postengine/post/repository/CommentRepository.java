7d13e4bf796e25af2fd91ed1cb542468e62efaab
wonnho71 <wonnho@nate.com>
2026-09-22 15:12:11 +0900

    add @Param('articleId') inside findByArticleId So Spring needs to know which Java parameter matches that name. Fix it

--- src/main/java/com/postengine/post/repository/CommentRepository.java
+++ src/main/java/com/postengine/post/repository/CommentRepository.java
@@ -3,13 +3,14 @@ package com.postengine.post.repository;
 import com.postengine.post.controller.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
 
     @Query(value="SELECT * FROM comment WHERE article_id=:articleId",nativeQuery = true)
-    List<Comment> findByArticleId(Long articleId);
+    List<Comment> findByArticleId(@Param("articleId") Long articleId);
 
     // retrieve all comments for a particular post
     List<Comment> findByNickname(String nickname);
